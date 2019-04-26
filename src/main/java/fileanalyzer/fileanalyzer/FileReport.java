5dccbae74988850b4b3036e107683b0aa8ac069c
Agustín Gianni Borello <agustinborello21@gmail.com>
2019-04-25 21:27:35 -0300

    Update FileReport.java

--- src/main/java/fileanalyzer/fileanalyzer/FileReport.java
+++ src/main/java/fileanalyzer/fileanalyzer/FileReport.java
@@ -1,5 +1,8 @@
 package fileanalyzer.fileanalyzer;
-
+/*
+Esta clase deberia recibir el json del reporte de virus total
+y dejarlo en formatito mas pasable para mostrarlo por consola
+*/
 public class FileReport {
     private final String rawJson;
     private final boolean success;
